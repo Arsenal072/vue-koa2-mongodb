9603333ee153d0c70ce3b19db673b5aba717ce75
yellow <zjutGQchen@163.com>
2020-12-09 19:27:00 +0800

    update: 样式修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,100 +37,103 @@
 
         </div>
         <div class="bottom-box">
-            <div>&copy; 
-                <span>GQchen</span> 
-                <span >2019-08-30</span>
+            <div>&copy;
+                <span>GQchen</span>
+                <span>2019-08-30</span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: "Home",
-    components: {
-    },
-    data(){
-        return {
-            userInfo: {},
-            form: {
-                username: '',
-                password: '',
-                confirmPassword: ''
+    export default {
+        name: "Home",
+        components: {},
+        data() {
+            return {
+                userInfo: {},
+                form: {
+                    username: '',
+                    password: '',
+                    confirmPassword: ''
+                }
             }
-        }
-    },
-    methods: {
-        login(){
-            this.$router.push('/login')
         },
-        register(){
-            this.$router.push('/register') 
-        },
-        logout(){
+        methods: {
+            login() {},
+            register() {},
+            logout() {
 
-        },
-        onSubmit() {
-            console.log('submit!');
+            },
+            onSubmit() {
+                console.log('submit!');
+            }
         }
     }
-}
 </script>
 <style lang="scss">
-.home-wrapper {
-    .outer-wrapper {
-        display: flex;
-        justify-content: space-between;
-        height: 60px;
-        line-height: 60px;
-        padding: 0 50px;
-        color: #fff;
-        background-color: #3a5fcd;
-        .iconfont {
-            font-size: 24px;
-        }
-        .left-box{
-            .iconfont{
-                padding-right: 10px;
+    .home-wrapper {
+        .outer-wrapper {
+            display: flex;
+            justify-content: space-between;
+            height: 60px;
+            line-height: 60px;
+            padding: 0 50px;
+            color: #fff;
+            background-color: #3a5fcd;
+
+            .iconfont {
+                font-size: 24px;
             }
-        }
-        .right-box {
-            padding-right: 100px;
-            .quit-box {
-                padding-left: 20px;
-                font-size: 18px;
-                display: inline-block;
-                cursor: pointer;
-                .icon-tuichu {
+
+            .left-box {
+                .iconfont {
+                    padding-right: 10px;
+                }
+            }
+
+            .right-box {
+                padding-right: 100px;
+
+                .quit-box {
+                    padding-left: 20px;
                     font-size: 18px;
+                    display: inline-block;
+                    cursor: pointer;
+
+                    .icon-tuichu {
+                        font-size: 18px;
+                    }
                 }
             }
         }
-    }
-    .main-content{
-        width: 80%;
-        margin: 0 auto;
-        .login-modal{
-            position: relative;
-            top: 100px;
-            float: right;
-            width: 300px;
-            border: 1px solid #eee;
-            padding: 20px 30px 10px;
-            background-color: #eee;
+
+        .main-content {
+            width: 80%;
+            margin: 0 auto;
+
+            .login-modal {
+                position: relative;
+                top: 100px;
+                float: right;
+                width: 300px;
+                border: 1px solid #eee;
+                padding: 20px 30px 10px;
+                background-color: #eee;
+            }
+
+            background: url('/assets/images/background.gif') no-repeat center top;
         }
 
-        background: url('/assets/images/background.gif') no-repeat center top;
+        .bottom-box {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px;
+            font-size: 20px;
+            line-height: 60px;
+            background-color: #3A5FCD;
+            color: #fff;
+        }
     }
-    .bottom-box{
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        height: 60px;
-        font-size: 20px;
-        line-height: 60px;
-        background-color: #3A5FCD;
-        color: #fff;
-    }
-}
 </style>
